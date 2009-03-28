44e2b4bbf451e78a351505e98d22a173a5a3e893
Yanick Champoux <yanick@babyl.dyndns.org>
2009-03-28 17:27:31 -0400

    housekeeping for rel v0.1.5

--- lib/Git/CPAN/Patch.pm
+++ lib/Git/CPAN/Patch.pm
@@ -3,7 +3,7 @@ package Git::CPAN::Patch;
 use strict;
 use warnings;
 
-our $VERSION = '0.1.4';
+our $VERSION = '0.1.5';
 
 __END__
 
@@ -100,7 +100,7 @@ Report the managed module
 
 =head1 VERSION
 
-This document describes Git::CPAN::Patch version 0.1.4
+This document describes Git::CPAN::Patch version 0.1.5
 
 =head1 BUGS AND LIMITATIONS
 
