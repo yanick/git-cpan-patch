fdbff2c768fa2092fa5c3035501693cec0a5b547
Yanick Champoux <yanick+cpan@babyl.dyndns.org>
2009-05-12 16:58:40 -0800

    import Git-CPAN-Patch 0.1.6 from CPAN
    git-cpan-module:   Git-CPAN-Patch
    git-cpan-version:  0.1.6
    git-cpan-authorid: YANICK
    git-cpan-file:     authors/id/Y/YA/YANICK/Git-CPAN-Patch-0.1.6.tar.gz

--- lib/Git/CPAN/Patch.pm
+++ lib/Git/CPAN/Patch.pm
@@ -3,7 +3,7 @@ package Git::CPAN::Patch;
 use strict;
 use warnings;
 
-our $VERSION = '0.1.5';
+our $VERSION = '0.1.6';
 
 __END__
 
@@ -100,7 +100,7 @@ Report the managed module
 
 =head1 VERSION
 
-This document describes Git::CPAN::Patch version 0.1.5
+This document describes Git::CPAN::Patch version 0.1.6
 
 =head1 BUGS AND LIMITATIONS
 
