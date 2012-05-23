c3ad6214fb32332453e2b6381fbabfbe940a0916
Mike Doherty <doherty@cs.dal.ca>
2012-05-23 09:28:01 -0300

    Update main docs for new cpan-clone command

--- lib/Git/CPAN/Patch.pm
+++ lib/Git/CPAN/Patch.pm
@@ -68,9 +68,8 @@ Git::CPAN::Patch - Patch CPAN modules using Git
 
     # import a module:
 
-    % mkdir Foo-Bar
+    % git cpan-clone Foo::Bar
     % cd Foo-Bar
-    % git cpan-init Foo::Bar
 
 
 
@@ -93,27 +92,31 @@ Git::CPAN::Patch - Patch CPAN modules using Git
 =head1 DESCRIPTION
 
 L<Git::CPAN::Patch> provides a suite of git commands
-aimed at making trivially 
-easy the process of  grabbing 
-any distribution off CPAN, stuffing it 
+aimed at making trivially
+easy the process of  grabbing
+any distribution off CPAN, stuffing it
 in a local git repository and, once gleeful
 hacking has been perpetrated, sending back
-patches to its maintainer.  
+patches to its maintainer.
 
 =head1 GIT COMMANDS
 
 =over
 
-=item L<git-cpan-init>    
+=item L<git-cpan-clone>
+
+Clone a CPAN module's history into a new git repository
+
+=item L<git-cpan-init>
 
 Create a git repository for a CPAN module
 
-=item L<git-backpan-init> 
+=item L<git-backpan-init>
 
 Initialize a repository for a CPAN module with full history
 from the backpan.
 
-=item L<git-cpan-import>  
+=item L<git-cpan-import>
 
 Import a module into a git repository.
 
@@ -121,11 +124,11 @@ Import a module into a git repository.
 
 Report the last imported version
 
-=item L<git-cpan-send-email>    
+=item L<git-cpan-send-email>
 
 Use C<git-send-email> to submit patches to CPAN RT
 
-=item L<git-cpan-sendpatch>  
+=item L<git-cpan-sendpatch>
 
 Create patch files and submit then to RT
 
@@ -133,7 +136,7 @@ Create patch files and submit then to RT
 
 Import the latest version of a module and rebase the current branch
 
-=item L<git-cpan-format-patch>  
+=item L<git-cpan-format-patch>
 
 Format patches using C<cpan/master> as the origin reference
 
@@ -141,7 +144,7 @@ Format patches using C<cpan/master> as the origin reference
 
 Combine multiple commits into one patch
 
-=item L<git-cpan-which> 
+=item L<git-cpan-which>
 
 Report the managed module
 
@@ -151,10 +154,10 @@ Report the managed module
 =head1 BUGS AND LIMITATIONS
 
 Please report any bugs or feature requests to
-C<bug-git-cpan-patch@rt.cpan.org>, or through the web 
+C<bug-git-cpan-patch@rt.cpan.org>, or through the web
 interface at L<http://rt.cpan.org>.
 
-  
+
 =head1 AUTHORS
 
 Yanick Champoux C<< <yanick@cpan.org> >>
@@ -172,34 +175,36 @@ modify it under the same terms as Perl itself. See L<perlartistic>.
 
 =over
 
-=item L<git-backpan-init>       
+=item L<git-backpan-init>
 
-=item L<git-cpan-import>  
+=item L<git-cpan-clone>
+
+=item L<git-cpan-import>
 
 =item L<git-cpan-last-version>
 
-=item L<git-cpan-sendpatch>  
+=item L<git-cpan-sendpatch>
 
 =item L<git-cpan-update>
 
-=item L<git-cpan-format-patch>  
+=item L<git-cpan-format-patch>
 
-=item L<git-cpan-init>    
+=item L<git-cpan-init>
 
-=item L<git-cpan-send-email>    
+=item L<git-cpan-send-email>
 
 =item L<git-cpan-squash>
 
 =item L<git-cpan-which>
 
-=back 
+=back
 
 
 =head2 Articles
 
-The set of scripts that would eventually become 
-L<Git::CPAN::Patch> were first presented in the 
-article I<CPAN Patching with Git>, published in 
+The set of scripts that would eventually become
+L<Git::CPAN::Patch> were first presented in the
+article I<CPAN Patching with Git>, published in
 issue 5.1 of L<The Perl Review|http://theperlreview.com>.
 
 =head2 Git::CPAN::Patch on the Net
@@ -218,7 +223,7 @@ http://rt.cpan.org/Public/Dist/Display.html?Name=Git-CPAN-Patch
 
 web interface: http://github.com/yanick/git-cpan-patch
 
-to clone:  
+to clone:
 
   $ git clone git://github.com/yanick/git-cpan-patch.git
 
